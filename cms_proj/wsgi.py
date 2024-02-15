e25ee54e5411da2713ca6661340755650597a3bd
Srinidhi Iyer <srinidhi.iyer18@gmail.com>
2024-02-15 14:36:32 +0530

    Deployment Commit

--- cms_proj/wsgi.py
+++ cms_proj/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cms_proj.settings')
+settings_module = 'cms_proj.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'cms_proj.settings'
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
